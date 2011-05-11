0e9d5b5828602d7d06deb499ad00c4c908505081
tdviet12@gmail.com <tdviet12@gmail.com@8d2011d2-114f-94d0-65d4-9cdb369e0c37>
2011-05-11 17:13:23 +0000

    Add Projetct

---
+++ trunk/src/0812611/t3vpaint/t3vpaint/ManHinhChinh.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace t3vpaint
+{
+    public partial class ManHinhChinh : Form
+    {
+        public ManHinhChinh()
+        {
+            InitializeComponent();
+        }
+
+        private void ManHinhChinh_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox form = new AboutBox();
+            form.Show();
+        }
+
+        
+    }
+}
---
+++ trunk/src/0812611/t3vpaint/t3vpaint/ManHinhDangNhap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace t3vpaint
+{
+    public partial class ManHinhDangNhap : Form
+    {
+        public ManHinhDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Huy_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
